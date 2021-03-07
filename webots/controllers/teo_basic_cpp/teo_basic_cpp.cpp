732cf1e3527354598e2455b0a37febc0ab25b6e5
jgvictores <jcgvicto@gmail.com>
2021-03-07 11:22:13 +0100

    init teo_basic_cpp

---
+++ webots/controllers/teo_basic_cpp/teo_basic_cpp.cpp
@@ -0,0 +1,42 @@
+// File:          teo_basic_cpp.cpp
+
+#include <iostream>
+
+#include <webots/Robot.hpp>
+
+using namespace webots;
+
+int main(int argc, char **argv)
+{
+  // create the Robot instance.
+  Robot *robot = new Robot();
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+  std::cout << timeStep << std::endl; // 32
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1)
+  {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+
+    // Enter here functions to send actuator commands, like:
+    //  motor->setPosition(10.0);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
